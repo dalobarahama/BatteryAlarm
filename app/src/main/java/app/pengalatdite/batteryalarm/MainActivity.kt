17ffbccb295c9b946d0ac335918ea0b12c4e5e9f
Dalo Barahama <das.barahama@gmail.com>
2021-04-04 23:21:30 +0800

    add charging status

--- app/src/main/java/app/pengalatdite/batteryalarm/MainActivity.kt
+++ app/src/main/java/app/pengalatdite/batteryalarm/MainActivity.kt
@@ -8,6 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import app.pengalatdite.batteryalarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val CHARGING = "Charging"
+        const val NOT_CHARGING = "Not Charging"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,6 +23,24 @@ class MainActivity : AppCompatActivity() {
                 baseContext.registerReceiver(null, intentFilter)
             }
 
+        getChargingStatus(binding, batteryStatus)
+        getChargingPercentage(binding, batteryStatus)
+
+        setContentView(binding.root)
+    }
+
+    private fun getChargingStatus(binding: ActivityMainBinding, batteryStatus: Intent?) {
+        val status: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+        val isCharging: Boolean = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL
+
+        if (isCharging)
+            binding.batteryStatus.text = CHARGING
+        else
+            binding.batteryStatus.text = NOT_CHARGING
+    }
+
+    private fun getChargingPercentage(binding: ActivityMainBinding, batteryStatus: Intent?) {
         val batteryPercentage: Float? = batteryStatus?.let { intent ->
             val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
             val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
@@ -24,7 +48,5 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.batteryPercentage.text = batteryPercentage.toString()
-
-        setContentView(binding.root)
     }
 }
